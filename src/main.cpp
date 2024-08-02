17faf86702caffd98611085e7b3e5339b968bf18
Sascha Schnug <sascha.schnug@flexis.de>
2024-08-02 09:31:20 +0200

    small example of issues with sparse matrix

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,152 @@
 #include <pybind11/pybind11.h>
+#include <pybind11/eigen.h>
+
+#include <Eigen/Dense>
+#include <Eigen/SparseCore>
+
+#include <iostream>
 
 #define STRINGIFY(x) #x
 #define MACRO_STRINGIFY(x) STRINGIFY(x)
 
-int add(int i, int j) {
-    return i + j;
+// OKAY BEHAVIOUR -> dense
+template <typename ScalarT>
+static void take_matrix(const Eigen::Array<ScalarT, Eigen::Dynamic, Eigen::Dynamic> matrix_in, int a) {
+  std::cout << "Dense overload" << std::endl;
+
+  if constexpr(std::is_same_v<ScalarT, float>) {
+    std::cout << "float" << std::endl;
+  }
+  else if constexpr(std::is_same_v<ScalarT, double>) {
+    std::cout << "double" << std::endl;
+  }
+  else if constexpr(std::is_same_v<ScalarT, bool>) {
+    std::cout << "bool" << std::endl;
+  }
+  else if constexpr(std::is_same_v<ScalarT, int8_t>) {
+    std::cout << "int8_t" << std::endl;
+  }
+  else if constexpr(std::is_same_v<ScalarT, int16_t>) {
+    std::cout << "int16_t" << std::endl;
+  }
+  else if constexpr(std::is_same_v<ScalarT, int32_t>) {
+    std::cout << "int32_t" << std::endl;
+  }
+  else if constexpr(std::is_same_v<ScalarT, int64_t>) {
+    std::cout << "int64_t" << std::endl;
+  }
+  else if constexpr(std::is_same_v<ScalarT, uint8_t>) {
+    std::cout << "uint8_t" << std::endl;
+  }
+  else if constexpr(std::is_same_v<ScalarT, uint16_t>) {
+    std::cout << "uint16_t" << std::endl;
+  }
+  else if constexpr(std::is_same_v<ScalarT, uint32_t>) {
+    std::cout << "uint32_t" << std::endl;
+  }
+  else if constexpr(std::is_same_v<ScalarT, uint64_t>) {
+    std::cout << "uint64_t" << std::endl;
+  }
+  else 
+    std::cout << "some other type" << std::endl;
+}
+
+// NOT OKAY BEHAVIOUR -> sparse
+template<typename ScalarT, int Options>
+static void take_matrix(const Eigen::SparseMatrix<ScalarT, Options> matrix_in, int a) {
+  std::cout << "Sparse overload" << std::endl;
+
+  if constexpr(std::is_same_v<ScalarT, float>) {
+    std::cout << "float" << std::endl;
+  }
+  else if constexpr(std::is_same_v<ScalarT, double>) {
+    std::cout << "double" << std::endl;
+  }
+  else if constexpr(std::is_same_v<ScalarT, bool>) {
+    std::cout << "bool" << std::endl;
+  }
+  else if constexpr(std::is_same_v<ScalarT, int8_t>) {
+    std::cout << "int8_t" << std::endl;
+  }
+  else if constexpr(std::is_same_v<ScalarT, int16_t>) {
+    std::cout << "int16_t" << std::endl;
+  }
+  else if constexpr(std::is_same_v<ScalarT, int32_t>) {
+    std::cout << "int32_t" << std::endl;
+  }
+  else if constexpr(std::is_same_v<ScalarT, int64_t>) {
+    std::cout << "int64_t" << std::endl;
+  }
+  else if constexpr(std::is_same_v<ScalarT, uint8_t>) {
+    std::cout << "uint8_t" << std::endl;
+  }
+  else if constexpr(std::is_same_v<ScalarT, uint16_t>) {
+    std::cout << "uint16_t" << std::endl;
+  }
+  else if constexpr(std::is_same_v<ScalarT, uint32_t>) {
+    std::cout << "uint32_t" << std::endl;
+  }
+  else if constexpr(std::is_same_v<ScalarT, uint64_t>) {
+    std::cout << "uint64_t" << std::endl;
+  }
+  else 
+    std::cout << "some other type" << std::endl;
 }
 
 namespace py = pybind11;
 
 PYBIND11_MODULE(cmake_example, m) {
-    m.doc() = R"pbdoc(
-        Pybind11 example plugin
-        -----------------------
 
-        .. currentmodule:: cmake_example
+    // dense
+    // -----
 
-        .. autosummary::
-           :toctree: _generate
+    m.def("take_matrix", &take_matrix<bool>, pybind11::arg("matrix_in").noconvert(), pybind11::arg("x"));
+    m.def("take_matrix", &take_matrix<float>, pybind11::arg("matrix_in").noconvert(), pybind11::arg("x"));
+    m.def("take_matrix", &take_matrix<double>, pybind11::arg("matrix_in").noconvert(), pybind11::arg("x"));
+    m.def("take_matrix", &take_matrix<int8_t>, pybind11::arg("matrix_in").noconvert(), pybind11::arg("x"));
+    m.def("take_matrix", &take_matrix<int16_t>, pybind11::arg("matrix_in").noconvert(), pybind11::arg("x"));
+    m.def("take_matrix", &take_matrix<int32_t>, pybind11::arg("matrix_in").noconvert(), pybind11::arg("x"));
+    m.def("take_matrix", &take_matrix<int64_t>, pybind11::arg("matrix_in").noconvert(), pybind11::arg("x"));
+    m.def("take_matrix", &take_matrix<uint8_t>, pybind11::arg("matrix_in").noconvert(), pybind11::arg("x"));
+    m.def("take_matrix", &take_matrix<uint16_t>, pybind11::arg("matrix_in").noconvert(), pybind11::arg("x"));
+    m.def("take_matrix", &take_matrix<uint32_t>, pybind11::arg("matrix_in").noconvert(), pybind11::arg("x"));
+    m.def("take_matrix", &take_matrix<uint64_t>, pybind11::arg("matrix_in").noconvert(), pybind11::arg("x"));
 
-           add
-           subtract
-    )pbdoc";
+    // sparse
+    // ------
 
-    m.def("add", &add, R"pbdoc(
-        Add two numbers
+    m.def("take_matrix", &take_matrix<bool, Eigen::ColMajor>, pybind11::arg("matrix_in").noconvert(), pybind11::arg("x"));
+    m.def("take_matrix", &take_matrix<bool, Eigen::RowMajor>, pybind11::arg("matrix_in").noconvert(), pybind11::arg("x"));
 
-        Some other explanation about the add function.
-    )pbdoc");
+    m.def("take_matrix", &take_matrix<float, Eigen::ColMajor>, pybind11::arg("matrix_in").noconvert(), pybind11::arg("x"));
+    m.def("take_matrix", &take_matrix<float, Eigen::RowMajor>, pybind11::arg("matrix_in").noconvert(), pybind11::arg("x"));
 
-    m.def("subtract", [](int i, int j) { return i - j; }, R"pbdoc(
-        Subtract two numbers
+    m.def("take_matrix", &take_matrix<double, Eigen::ColMajor>, pybind11::arg("matrix_in").noconvert(), pybind11::arg("x"));
+    m.def("take_matrix", &take_matrix<double, Eigen::RowMajor>, pybind11::arg("matrix_in").noconvert(), pybind11::arg("x"));
 
-        Some other explanation about the subtract function.
-    )pbdoc");
+    m.def("take_matrix", &take_matrix<int8_t, Eigen::ColMajor>, pybind11::arg("matrix_in").noconvert(), pybind11::arg("x"));
+    m.def("take_matrix", &take_matrix<int8_t, Eigen::RowMajor>, pybind11::arg("matrix_in").noconvert(), pybind11::arg("x"));
+
+    m.def("take_matrix", &take_matrix<int16_t, Eigen::ColMajor>, pybind11::arg("matrix_in").noconvert(), pybind11::arg("x"));
+    m.def("take_matrix", &take_matrix<int16_t, Eigen::RowMajor>, pybind11::arg("matrix_in").noconvert(), pybind11::arg("x"));
+
+    m.def("take_matrix", &take_matrix<int32_t, Eigen::ColMajor>, pybind11::arg("matrix_in").noconvert(), pybind11::arg("x"));
+    m.def("take_matrix", &take_matrix<int32_t, Eigen::RowMajor>, pybind11::arg("matrix_in").noconvert(), pybind11::arg("x"));
+
+    m.def("take_matrix", &take_matrix<int64_t, Eigen::ColMajor>, pybind11::arg("matrix_in").noconvert(), pybind11::arg("x"));
+    m.def("take_matrix", &take_matrix<int64_t, Eigen::RowMajor>, pybind11::arg("matrix_in").noconvert(), pybind11::arg("x"));
+
+    m.def("take_matrix", &take_matrix<uint8_t, Eigen::ColMajor>, pybind11::arg("matrix_in").noconvert(), pybind11::arg("x"));
+    m.def("take_matrix", &take_matrix<uint8_t, Eigen::RowMajor>, pybind11::arg("matrix_in").noconvert(), pybind11::arg("x"));
+
+    m.def("take_matrix", &take_matrix<uint16_t, Eigen::ColMajor>, pybind11::arg("matrix_in").noconvert(), pybind11::arg("x"));
+    m.def("take_matrix", &take_matrix<uint16_t, Eigen::RowMajor>, pybind11::arg("matrix_in").noconvert(), pybind11::arg("x"));
+
+    m.def("take_matrix", &take_matrix<uint32_t, Eigen::ColMajor>, pybind11::arg("matrix_in").noconvert(), pybind11::arg("x"));
+    m.def("take_matrix", &take_matrix<uint32_t, Eigen::RowMajor>, pybind11::arg("matrix_in").noconvert(), pybind11::arg("x"));
+
+    m.def("take_matrix", &take_matrix<uint64_t, Eigen::ColMajor>, pybind11::arg("matrix_in").noconvert(), pybind11::arg("x"));
+    m.def("take_matrix", &take_matrix<uint64_t, Eigen::RowMajor>, pybind11::arg("matrix_in").noconvert(), pybind11::arg("x"));
 
 #ifdef VERSION_INFO
     m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,7 +1,32 @@
 import cmake_example as m
-
+import numpy as np
+import scipy as sp
 
 def test_main():
     assert m.__version__ == "0.0.1"
-    assert m.add(1, 2) == 3
-    assert m.subtract(1, 2) == -1
+
+    matrix = np.array(
+        [
+            [1, 2, 3, 4],
+            [5, 6, 7, 8]
+        ], dtype=np.float32
+    )
+    print('Dense matrix @ python')
+    print(matrix)
+    print(matrix.dtype)
+    print()
+
+    # -----
+
+    sparse_matrix = sp.sparse.coo_array(matrix)
+    print('Sparse matrix @ python')
+    print(sparse_matrix)
+    print(sparse_matrix.dtype)
+    print()
+
+    # -----
+
+    print('CALL instantiation -> with dense => c++ receives type?')
+    m.take_matrix(matrix, True)
+    print('CALL instantiation -> with sparse => c++ receives type?')
+    m.take_matrix(sparse_matrix, True)
